da4b119d14e80ac993b2dbed88c616dd84e328cc
Whw Whw <hrhrhrrhhwhw@gmail.com>
2025-12-30 01:48:57 +0400

    Add form.js

--- api/form.js
+++ api/form.js
@@ -1,14 +1,9 @@
-import express from "express";
-import cors from "cors";
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.post("/", async (req, res) => {
   console.log("FORM DATA:", req.body);
-  res.status(200).json({ success: true });
-});
 
-export default app;
+  return res.status(200).json({ success: true });
+}
