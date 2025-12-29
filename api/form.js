b368c8f9b3ca18b6eeb4bc1a1aaf73e76d62f016
Whw Whw <hrhrhrrhhwhw@gmail.com>
2025-12-30 02:18:39 +0400

    Add file's

--- api/form.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
-
-  return res.status(200).json({ success: true });
-}
