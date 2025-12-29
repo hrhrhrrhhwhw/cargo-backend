939fb815875384634d0fda88e0862420cec02004
Whw Whw <hrhrhrrhhwhw@gmail.com>
2025-12-30 01:55:58 +0400

    Fix Vercel entrypoint using serverless functions

--- api/form.js
+++ api/form.js
@@ -3,7 +3,5 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  console.log("FORM DATA:", req.body);
-
   return res.status(200).json({ success: true });
 }
