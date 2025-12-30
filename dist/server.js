219a2a5eb361cbe87780cf14090c29ae4c718b5c
Whw Whw <hrhrhrrhhwhw@gmail.com>
2025-12-30 10:26:31 +0400

    Redeploy-3

---
+++ dist/server.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
+const grammy_1 = require("grammy");
+const bot = new grammy_1.Bot(process.env.BOT_TOKEN);
+async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method Not Allowed" });
+    }
+    try {
+        const { departure, arrive, cargo, wagonType, email } = req.body;
+        if (!departure || !arrive || !cargo || !wagonType || !email) {
+            return res.status(400).json({ error: "Invalid data" });
+        }
+        const text = `
+📨 <b>Новая заявка</b>
+
+🚉 <b>Отправление:</b> ${departure}
+🏁 <b>Назначение:</b> ${arrive}
+📦 <b>Груз:</b> ${cargo}
+🚋 <b>Тип вагона:</b> ${wagonType}
+📧 <b>Email:</b> ${email}
+`;
+        await bot.api.sendMessage(process.env.CHAT_ID, text, { parse_mode: "HTML" });
+        return res.status(200).json({ ok: true });
+    }
+    catch (error) {
+        console.error("Telegram error:", error);
+        return res.status(500).json({ error: "Telegram error" });
+    }
+}
